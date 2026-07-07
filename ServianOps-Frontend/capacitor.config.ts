73cb743a3df5abdbb5a8102be5494c442fe8033e
Shalin <shalin_jirawla@hotmail.com>
2026-07-07 18:11:51 +0530

    Initial commit with .NET and Angular project structure

---
+++ ServianOps-Frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.servian.ops',
+  appName: 'ServianOps',
+  webDir: 'dist'
+};
+
+export default config;
